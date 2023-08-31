b1a84dd41d11e9d07ada87c29996f49a5b0cd470
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-08-30 23:47:18 -0400

    [config] setup

---
+++ components/Navbar.vue
@@ -0,0 +1,5 @@
+<template lang="">
+  <nav>
+    
+  </nav>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+  </div>
+</template>
+<script lang="ts">
+
+</script>
+<style lang="css">
+  
+</style>
