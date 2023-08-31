425acb8422e90e04f95fa05ae37b02bb9710b8ba
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-08-31 19:32:02 -0400

    [config] fontaine ready, formkit install, navbar simple component, and login directory

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,7 @@
-<template lang="">
-  <nav>
-    
-  </nav>
+<template>
+  <div class="flex justify-end items-center w-full h-14">
+    <button @click="" class="h-10 font-medium px-3 mr-2 py-2 bg-[#055ef8] rounded-lg text-white">Iniciar Sesion</button>
+    <button @click="" class="h-10 font-medium px-3 mr-2 py-2 bg-red-500 rounded-lg text-white">Registrarme</button>
+  </div>
 </template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-    <h1>Hello</h1>
-  </div>
+  <Navbar />
+  
 </template>
 <script lang="ts">
 
 </script>
 <style lang="css">
-  
+@font-face {
+  font-family: 'Roboto';
+  font-display: swap;
+  src: url('/fonts/Roboto.woff2') format('woff2'), url('/fonts/Roboto.woff') format('woff');
+  font-weight: 700;
+}
 </style>
